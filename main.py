fbba0cfaa9b0f2ba5176a39ac1450c7d9aae4f7b
Daniil2022 <paradana2@gmail.com>
2023-01-17 19:05:14 +0200

    Version 1.1

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def generator():
-    for _ in [34, 234, 102]:
-        yield
+    for i in [1, 25, 333, 1018]:
+        yield i
 
-gen_iterator = generator().__iter__()
-for _ in range(1, 4):
-    print(gen_iterator.__next__())
 
-print(type(gen_iterator))
-print(iter([34, 234, 102]))
+iterrator = generator().__iter__()
+for i in range(1, 5):
+    print(iterrator.__next__())
+print(iter([1, 25, 333, 1018]))
